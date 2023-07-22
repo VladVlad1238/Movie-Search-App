3af33b82563a4dca210edb5e94c542a9932b40c5
Vladyslav Sikalo <vladislavmediol@gmail.com>
2023-07-22 23:56:44 +0200

    add close card event, add scroll bar download

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,24 @@
 const URL = 'https://www.omdbapi.com/?s=batman&apikey=ae86f325';
 const API = 'ae86f325';
 
+const BODY_OVERFLOW_FIXED = 'body_fixed';
 
-const inputNode = document.querySelector('#input');
-const formNode = document.querySelector('#form');
+const barEl = document.querySelector('.bar');
+const bodyNode = document.querySelector('body');
+const headerNode = document.querySelector('.header');
+
+const formNode = document.getElementById('form');
+const inputNode = document.getElementById('input');
 const moviesEl = document.getElementById('movies');
 const movieCardNode = document.getElementById('movie__card-container');
+const closeButtonNode = document.getElementById('close-btn');
 
+const updateBar = () => {
+  let scrollPos = (window.scrollY / (bodyNode.scrollHeight - window.innerHeight)) * 100;
+  barEl.style.width = `${scrollPos}%`;
+  requestAnimationFrame(updateBar);
+};
+updateBar();
 
 const inputValue = () => inputNode.value;
 
@@ -14,10 +26,10 @@ const getDataFromUser = (e) => {
   e.preventDefault();
 
   const inputNodeValue = inputValue();
-  
+
   getMovies(inputNodeValue);
   clearInput();
-}
+};
 
 const clearInput = () => {
   inputNode.value = "";
@@ -29,7 +41,7 @@ const getMovies = async (inputNodeValue) => {
     const resp = await fetch(`https://www.omdbapi.com/?s=${inputNodeValue}&apikey=${API}`);
     const respData = await resp.json();
     showMovies(respData);
-    console.log(respData)
+    console.log(respData);
   } catch (error) {
     console.log(error);
   };
@@ -39,13 +51,12 @@ const showMovies = (data) => {
 
   moviesEl.innerHTML = '';
 
-  if(data.Response === 'True') {
+  if (data.Response === 'True') {
     data.Search.forEach((movie) => {
       const movieEl = document.createElement('div');
-     //movieEl.setAttribute('href', `html?movie.id${movie.imdbID}`);
-     movieEl.setAttribute('id', movie.imdbID);
+      movieEl.setAttribute('id', movie.imdbID);
       movieEl.classList.add('movies__wrapper');
-      movieEl.innerHTML =  `
+      movieEl.innerHTML = `
         <div class="movies__img-container">
           <img src="${movie.Poster}" alt="${movie.Title}" class="movie__img">
           </div>
@@ -56,62 +67,88 @@ const showMovies = (data) => {
           </div>
         </div>
       `
-
-      movieEl.addEventListener('click', () => showMovieInfo())
+      movieEl.addEventListener('click', async () => await getMovieInfo(movie.imdbID));
       moviesEl.appendChild(movieEl);
-      
     });
   } else {
     const errorMessage = document.createElement('p');
     errorMessage.innerText = 'This movie is not definded';
     moviesEl.appendChild(errorMessage);
-  }
-  getMovieInfo()
+  };
 };
 
-const getMovieInfo = () => {
-  const searchMovieInfo = moviesEl.document.querySelectorAll('.movies-wrapper');
-  searchMovieInfo.forEach((movie) => {
-    movie.addEventListener('click', async () =>{
-      const url = `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=ae86f325`;
-      const resp = await fetch(`${url}`);
-      const info = await resp.json();
-      showMovieInfo(info);
-    })
-  })
-}
+const getMovieInfo = async (movieID) => {
+  try {
+    const url = `https://www.omdbapi.com/?i=${movieID}&apikey=ae86f325`;
+    const resp = await fetch(`${url}`);
+    const info = await resp.json();
+    showMovieInfo(info);
+  } catch (error) {
+    console.log(error);
+  };
+};
 
-const showMovieInfo = (movieInfo) => {
-  const movieCardEl = document.createElement('div')
-  movieCardEl.classList.add('movie__card-wrapper');
-  movieCardEl.classList.add('movie__card-container--show');
+const showMovieInfo = (data) => {
+  const movieCardEl = document.createElement('div');
+  movieCardEl.classList.add('movie__card');
+
   movieCardEl.innerHTML = `
-  <div class="movie__card-wrapper">
-  <div id="movie__card" class="movie__card">
-      <div class="movie__card-img">
-          <img class="card__img" src="${movieInfo.Poster}" alt="">
-      </div>
-      <div class="movie__card-content-container">
-          <h2 class="movie__card-title">${movieInfo.Title}</h2>
-          <p class="movie__year">Year: <span>${movieInfo.Year}</span></p>
-          <p class="movie__rated">Rated: <span></span></p>
-          <p class="movie__released">Released: <span></span></p>
-          <p class="movie__runtime">Runtime: <span></span></p>
-          <p class="movie__genre">Genre: <span></span></p>
-          <p class="movie__director">Director: <span></span></p>
-          <p class="movie__writer">Writer: <span></span></p>
-          <p class="movie__actors">Actors: <span></span></p>
+      <div class="inner-wrapper">
+        <div class="movie__card-img">
+          <img class="card__img" src="${data.Poster}" alt="">
+        </div>
+        <div class="movie__card-content-container">
+          <h2 class="movie__card-title">${data.Title}</h2>
+          <p class="movie__year">Year: <span>${data.Year}</span></p>
+          <p class="movie__rated">Rated: <span>${data.Rated}</span></p>
+          <p class="movie__released">Released: <span>${data.Released}</span></p>
+          <p class="movie__runtime">Runtime: <span>${data.Runtime}</span></p>
+          <p class="movie__genre">Genre: <span>${data.Genre}</span></p>
+          <p class="movie__director">Director: <span>${data.Director}</span></p>
+          <p class="movie__writer">Writer: <span>${data.Writer}</span></p>
+          <p class="movie__actors">Actors: <span>${data.Actors}</span></p>
+        </div>
       </div>
       <div class="movie__card-info">
-          <p class="movie__text"><span></span></p>
+        <p class="movie__text"><span>${data.Plot}</span></p>
       </div>
-      <button id="close-btn" class="movie__card-close-btn">
-          <img src="/img/close-circle-svgrepo-com (1).svg" alt="" class="close__button-img">
+      <button id="close-btn" class="movie__card-close-btn type="button" data-action="close">
+        <img src="img/close-circle-svgrepo-com (1).svg" alt="" class="close__button-img">
       </button>
-  </div>
-</div>`
-movieCardNode.appendChild(movieCardEl)
-}
+` 
 
+  movieCardNode.appendChild(movieCardEl);
+  movieCardHandler();
+};
 
+const movieCardHandler = () => {
+  movieCardNode.classList.add('movie__card-container--show');
+  bodyNode.classList.add('body_fixed');
+  headerNode.classList.add('header__hidden');
+  moviesEl.classList.add('movie__list-hidden');
+};
+
+const closeModal = () => {
+  bodyNode.classList.remove('body_fixed');
+  movieCardNode.classList.remove('movie__card-container--show');
+  headerNode.classList.remove('header__hidden');
+  moviesEl.classList.remove('movie__list-hidden');
+};
+
+bodyNode.addEventListener('click', (e) => {
+  if(e.target.id === "close-btn") {
+    closeModal();
+  };
+});
+
+window.addEventListener('click', (e) => {
+  if(e.target === movieCardNode) {
+    closeModal();
+  };
+});
+
+movieCardNode.addEventListener('click', movieCardHandler);
 formNode.addEventListener('submit', getDataFromUser);
+
+
+//переделать имена переменных и событий 
