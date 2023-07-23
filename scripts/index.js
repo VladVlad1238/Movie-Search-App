015ec495b89b1b6c9ba2e1d55b67b9b2d19de8fb
Vladyslav Sikalo <vladislavmediol@gmail.com>
2023-07-23 19:57:50 +0200

    last update

--- scripts/index.js
+++ scripts/index.js
@@ -2,54 +2,60 @@ const URL = 'https://www.omdbapi.com/?s=batman&apikey=ae86f325';
 const API = 'ae86f325';
 
 const BODY_OVERFLOW_FIXED = 'body_fixed';
+const ERROR_MESSAGE_TEXT = 'This movie is not definded';
 
-const barEl = document.querySelector('.bar');
+const barElNode = document.querySelector('.bar');
 const bodyNode = document.querySelector('body');
 const headerNode = document.querySelector('.header');
 
 const formNode = document.getElementById('form');
 const inputNode = document.getElementById('input');
-const moviesEl = document.getElementById('movies');
+const moviesElNode = document.getElementById('movies__list');
 const movieCardNode = document.getElementById('movie__card-container');
-const closeButtonNode = document.getElementById('close-btn');
+const closeButtonNode = document.getElementById('movie__card-close-btn');
 
+//функция горизонтального скролла
 const updateBar = () => {
   let scrollPos = (window.scrollY / (bodyNode.scrollHeight - window.innerHeight)) * 100;
-  barEl.style.width = `${scrollPos}%`;
+  barElNode.style.width = `${scrollPos}%`;
   requestAnimationFrame(updateBar);
 };
 updateBar();
 
+//получаем данные из инпута
 const inputValue = () => inputNode.value;
 
+//обрываем submit, передаем данные из инпута, выводим список фильмов/информацию о фильме. Очищаем инпут
 const getDataFromUser = (e) => {
   e.preventDefault();
 
   const inputNodeValue = inputValue();
 
-  getMovies(inputNodeValue);
+  getMoviesList(inputNodeValue);
   clearInput();
 };
 
+//функция очистки инпута + фокус
 const clearInput = () => {
   inputNode.value = "";
   inputNode.focus();
 };
 
-const getMovies = async (inputNodeValue) => {
+//делаем запрос на серве и получаем данные которые передаем в функцию вывода списка/отлавливаем ошибки
+const getMoviesList = async (inputNodeValue) => {
   try {
-    const resp = await fetch(`https://www.omdbapi.com/?s=${inputNodeValue}&apikey=${API}`);
+    const url = `https://www.omdbapi.com/?s=${inputNodeValue}&apikey=${API}`;
+    const resp = await fetch(url);
     const respData = await resp.json();
-    showMovies(respData);
-    console.log(respData);
+    showMoviesList(respData);
   } catch (error) {
     console.log(error);
   };
 };
 
-const showMovies = (data) => {
-
-  moviesEl.innerHTML = '';
+//функция отрисовки списка + асинхоронная функция замыкания которая получает айди фильма
+const showMoviesList = (data) => {
+  moviesElNode.innerHTML = '';
 
   if (data.Response === 'True') {
     data.Search.forEach((movie) => {
@@ -58,7 +64,7 @@ const showMovies = (data) => {
       movieEl.classList.add('movies__wrapper');
       movieEl.innerHTML = `
         <div class="movies__img-container">
-          <img src="${movie.Poster}" alt="${movie.Title}" class="movie__img">
+          <img  class="movies__img" src="${movie.Poster}" alt="${movie.Title}">
           </div>
           <div class="movies__content-container">
             <h2 class="movies__title">Name: <span>${movie.Title}</span></h2>
@@ -68,19 +74,20 @@ const showMovies = (data) => {
         </div>
       `
       movieEl.addEventListener('click', async () => await getMovieInfo(movie.imdbID));
-      moviesEl.appendChild(movieEl);
+      moviesElNode.appendChild(movieEl);
     });
   } else {
     const errorMessage = document.createElement('p');
-    errorMessage.innerText = 'This movie is not definded';
-    moviesEl.appendChild(errorMessage);
+    errorMessage.innerText = ERROR_MESSAGE_TEXT;
+    moviesElNode.appendChild(errorMessage);
   };
 };
 
+//получаем от сервера данные и конкретном фильме при помощи айди/отлавливаем ошибки
 const getMovieInfo = async (movieID) => {
   try {
     const url = `https://www.omdbapi.com/?i=${movieID}&apikey=ae86f325`;
-    const resp = await fetch(`${url}`);
+    const resp = await fetch(url);
     const info = await resp.json();
     showMovieInfo(info);
   } catch (error) {
@@ -88,6 +95,7 @@ const getMovieInfo = async (movieID) => {
   };
 };
 
+//функция отрисовки конкретного фильма
 const showMovieInfo = (data) => {
   const movieCardEl = document.createElement('div');
   movieCardEl.classList.add('movie__card');
@@ -112,43 +120,45 @@ const showMovieInfo = (data) => {
       <div class="movie__card-info">
         <p class="movie__text"><span>${data.Plot}</span></p>
       </div>
-      <button id="close-btn" class="movie__card-close-btn type="button" data-action="close">
-        <img src="img/close-circle-svgrepo-com (1).svg" alt="" class="close__button-img">
+      <button id="movie__card-close-btn" class="movie__card-close-btn type="button" data-action="close">
+        <img src="img/close-circle-svgrepo-com (1).svg" alt="" class="movie__card-close-button-img">
       </button>
 ` 
 
   movieCardNode.appendChild(movieCardEl);
-  movieCardHandler();
+  openMovieCard();
 };
 
-const movieCardHandler = () => {
+//открываем карточку фильма
+const openMovieCard = () => {
   movieCardNode.classList.add('movie__card-container--show');
   bodyNode.classList.add('body_fixed');
   headerNode.classList.add('header__hidden');
-  moviesEl.classList.add('movie__list-hidden');
+  moviesElNode.classList.add('movies__list-hidden');
 };
 
-const closeModal = () => {
+//закрываем карточку фильма
+const closeMovieCard = () => {
   bodyNode.classList.remove('body_fixed');
   movieCardNode.classList.remove('movie__card-container--show');
   headerNode.classList.remove('header__hidden');
-  moviesEl.classList.remove('movie__list-hidden');
+  moviesElNode.classList.remove('movies__list-hidden');
 };
 
+//событие закрытия карточки фильма по клику на кнопку
 bodyNode.addEventListener('click', (e) => {
-  if(e.target.id === "close-btn") {
-    closeModal();
+  if(e.target.id === "movie__card-close-btn") {
+    closeMovieCard();
   };
 });
 
+//событие для закрытия карточки фильма, если клик был за пределами карточки
 window.addEventListener('click', (e) => {
   if(e.target === movieCardNode) {
-    closeModal();
+    closeMovieCard();
   };
 });
 
-movieCardNode.addEventListener('click', movieCardHandler);
+movieCardNode.addEventListener('click', openMovieCard);
 formNode.addEventListener('submit', getDataFromUser);
 
-
-//переделать имена переменных и событий 
